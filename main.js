f61a455ff4276593de0bf718f34744febe926cee
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-12 18:19:32 +0200

    Milestone 1 fatta

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  // parametri query e api_key e language=it-IT
  //  Creare un layout base con una searchbar (una input e un button) in cui possiamo
- // scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
+ // scrivere completamente o parzialmente il nome di un film.  Cliccando il
  // bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
  // Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
  // film trovato:
@@ -23,11 +23,12 @@
        axios.get('https://api.themoviedb.org/3/search/movie', {
          params: {
            api_key: '0019fe3454f31b1558a9dfb6c203ad5b',
-           query: 'this.title',
+           query: this.title,
            language: 'it-IT'
          }
-       }).then((response)=>{
+       }).then((response) => {
          this.films = response.data.results
+         console.log(this.films);
        })
 
      }
