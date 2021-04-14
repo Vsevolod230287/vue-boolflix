7a2a91f8d829f094323b56998d14f871cb28e8a5
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-14 22:44:31 +0200

    Fatto menu dropdown in mobile version al ckick

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var app = new Vue({
 
   el: '#root',
   data: {
+    menu: 'su',
     x: false,
     uriMovie: 'https://api.themoviedb.org/3/search/movie',
     uriTv: 'https://api.themoviedb.org/3/search/tv',
@@ -25,40 +26,50 @@ var app = new Vue({
         .then((response) => {
           this.films = [...this.films, ...response.data.results]
           this.x = false;
-          if(this.films.length == 0) {
+          if (this.films.length == 0) {
             this.x = true;
           }
         })
       axios.get(`${this.uriTv}?api_key=${this.api_key}&query=${this.inputTitle}&language=${this.language}`)
         .then((response) => {
-            this.films = [...this.films, ...response.data.results]
-            this.x = false;
-            if(this.films.length == 0) {
-              this.x = true;
-            }
+          this.films = [...this.films, ...response.data.results]
+          this.x = false;
+          if (this.films.length == 0) {
+            this.x = true;
+          }
 
         })
 
-    this.inputTitle = '';
-  },
-  getTitle: function(film) {
-    if (film.title) {
-      return film.title;
-    } else if (film.name) {
-      return film.name;
+      this.inputTitle = '';
+    },
+    getTitle: function(film) {
+      if (film.title) {
+        return film.title;
+      } else if (film.name) {
+        return film.name;
+      }
+    },
+    getOriginalTitle(film) {
+      if (film.title) {
+        return film.original_title;
+      } else if (film.name) {
+        return film.original_name;
+      }
+    },
+    getVote: function(film) {
+      return Math.ceil(film.vote_average / 2);
+    },
+    getDropMenu: function() {
+      if(this.menu === 'su'){
+        document.querySelector('ul').classList.add("show");
+        this.menu = 'giu';
+      }else {
+        document.querySelector('ul').classList.remove("show");
+        this.menu = 'su';
+      }
+
     }
-  },
-  getOriginalTitle(film) {
-    if (film.title) {
-      return film.original_title;
-    } else if (film.name) {
-      return film.original_name;
-    }
-  },
-  getVote: function(film) {
-    return Math.ceil(film.vote_average / 2);
+
+
   }
-
-
-}
 })
